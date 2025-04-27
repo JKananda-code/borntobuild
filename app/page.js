52db4a3ed6b5812eab05b595537cadc1478bb05a
unknown <jkananda5@outlook.com>
2025-04-27 17:36:14 -0600

    changes PM today 26-04-2025

--- app/page.js
+++ app/page.js
@@ -8,10 +8,10 @@ const HomePage = () => {
     <div className="relative w-full h-64 md:h-96">
       <div className="flex flex-wrap justify-center">
       <div className="absolute inset-0 bg-blue-500 bg-opacity-50 flex flex-col justify-center items-center text-center text-white px-4">
-        <h1 className="text-xl md:text-2xl font-bold mb-2">
+        <h1 className="text-base md:text-2xl font-bold mb-2">
           Paticipating in our Charity Events is Compassion in Action
         </h1>
-        <div className="text-lg md:text-2xl">
+        <div className="text-sm md:text-2xl">
         Dear Members, Partners, and Well-Wishers,
         We extend our heartfelt gratitude to each one of you for your incredible support during our CBO Charity Event at Salem Rescue Center, Maraba, Thika, on April 13th, 2025. Your generous contributions—whether in cash or kind—played an essential role in making the event a resounding success.
         As Born To Build CBO, we remain deeply thankful for your unwavering commitment to this noble cause of uplifting the needy in our community. Your kindness and support have left a lasting impact, and we will cherish your contributions forever.
