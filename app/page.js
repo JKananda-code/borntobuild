56c9cfe80b771d1e77dfa84967b103f2424caa27
unknown <jkananda5@outlook.com>
2025-04-27 19:15:39 -0600

    changes PM today 26-04-2025

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ const HomePage = () => {
     <div className="relative w-full h-64 md:h-96">
       <div className="flex flex-wrap justify-center">
       <div className="absolute inset-0 bg-blue-500 bg-opacity-50 flex flex-col justify-center items-center p-4 text-white overflow-y-auto m-4">
-        <h1 className="text-sm md:text-2xl font-bold">
+        <h1 className="text-sm md:text-2xl font-bold mb-2">
           Paticipating in our Charity Events is Compassion in Action
         </h1>
         <div className="text-sm md:text-2xl">
