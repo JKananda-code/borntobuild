e4241b3febdad3e527a5610dde15989cc9802b8d
unknown <jkananda5@outlook.com>
2025-04-27 21:00:16 -0600

    changes PM today 26-04-2025

--- app/Activities/page.js
+++ app/Activities/page.js
@@ -7,11 +7,19 @@ function Activities
 () {
   return (
 <div className="flex justify-center">
-<div className="text-2xl font-bold mb-6 text-center">
+<div className="text-2xl font-bold mb-6 text-center space-y-2">
     Kiambu County
        <Image
         src="/kiambu-county-map.png"
         alt="Map of Kiambu County"
+        width={1200}
+        height={800}
+        priority
+        className="mx-auto"
+         />
+         <Image
+        src="/Activities-0.jpg"
+        alt="Guiding Principles"
         width={1200}
         height={800}
         priority
