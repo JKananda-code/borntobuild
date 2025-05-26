cd8b90bd7dfbd39ad1af623f12f44ad9a8fd5d71
unknown <jkananda5@outlook.com>
2025-05-25 23:43:59 -0600

    change bg=color social links to blueh

--- app/components/SocialLinks.js
+++ app/components/SocialLinks.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 function SocialLinks(){
     return(
-        <div className="flex gap-4 justify-center">
+        <div className="flex gap-4 justify-center bg-blue-500">
             <Link href="https://facebook.com/yourhandle" target="_blank" rel="nonopener noreferrer">Facebook</Link>
             <Link href="https://twitter.com/yourhandle" target="_blank" rel="nonopener noreferrer">Twitter</Link>
             <Link href="https://instagram.com/yourhandle"target="_blank" rel="nonopener noreferrer">Instagram</Link>
